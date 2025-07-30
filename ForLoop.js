8d10aeb30ae6e0faecd10fb1d15ba72dbf805dee
Mtanvir <tanviranjum201@gmail.com>
2025-07-30 21:44:25 +0600

    For loop

---
+++ ForLoop.js
@@ -0,0 +1,15 @@
+const numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+for(const num of numbers){
+    console.log(num);
+    
+}
+
+const fruits=['Mango', 'Apple', 'Jackfruit', 'Banana'];
+console.log(fruits);
+for(const fruit of fruits)
+{
+    console.log(fruit);
+    
+}
+
